3200107dca0f8f9b289e69aedb99e8269996f0ed
mdtaosifhossain3 <mdtaosifhossain3@gmail.com>
2025-09-23 23:49:04 +0600

    utils

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
+// Load environment variables from .env file
 dotenv.config();
 
 // Connect to MongoDB
-connectDB();
+connectDB()
+  .then(() => {
+    // Start the Express server
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
 
-// (async () => {
-//   try {
-//     await mongoose.connect(`${MONGO_URL}/${DB_NAME}`, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("Connected to MongoDB");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//   }
-// })();
+    app.on("error", (error) => {
+      console.error("Express server error:", error);
+    });
+  })
+  .catch((error) => {
+    console.error("Error starting server:", error);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+//Higher Order function. A function that received function as a pharameter also call it. ex: const fo = (fn)=> ()=> {}
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+//2nd way
+// const asyncHandlerSecondWay = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       message: error.message,
+//       success: false,
+//     });
+//   }
+// };
+export { asyncHandler };
